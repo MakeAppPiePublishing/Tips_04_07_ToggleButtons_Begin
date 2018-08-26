9c010f555e91a38683ffcf4975e880cdbef5dee6
Steve Lipton <steve@makeapppie.com>
2018-08-26 16:31:47 -0500

    Initial Commit

---
+++ buttons and switches/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  buttons and switches
+//
+//  Created by Steven Lipton on 8/26/18.
+//  Copyright © 2018 Steven Lipton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
